50467a7f19bdbb9a311e2e91af4097ec6413c64f
LuisParedes <luisparedes1009@hotmail.com>
2022-12-22 17:26:49 -0500

    good

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,12 @@
 import AddListing from "../components/AddListing"
 import { Link } from "react-router-dom"
-
+import myImg from '../assets/dining-table-bbq.jpg'
 
 function HomePage(){
     return (
         <main>
          <h1>RENT<span className="titleSpace"></span> A <span className="titleSpace"></span>YARD</h1>
-        <img className="homeImage" src="/src/assets/dining-table-bbq.jpg"/>
+        <img className="homeImage" src={myImg}/>
         
            
             <p>Perhaps you don’t have access to a backyard of your own, or maybe you’re just looking for a change of scenery or just a simple BBQ day. Whatever your reason for doing so, it’s surprisingly easy to rent a backyard and a grill for a day. Discover where and how to do so as well as a few examples of backyards that are available to rent in your area</p>
